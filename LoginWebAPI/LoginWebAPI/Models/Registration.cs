eaae793482da809e49fd9afc9cf7d4d564ea4f90
truong07 <truongtn7122003@gmail.com>
2024-05-09 09:57:26 +0700

    create database and conntect

---
+++ LoginWebAPI/LoginWebAPI/Models/Registration.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginWebAPI.Models
+{
+    public class Registration
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public required string Name { get; set; }
+        [Required]
+        public required string Email { get; set; }
+        [Required]
+        public required string Password { get; set; }
+    }
+}
---
+++ LoginWebAPI/LoginWebAPI/Models/RegistrationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LoginWebAPI.Models
+{
+    public class RegistrationDbContext : DbContext
+    {
+        public RegistrationDbContext(DbContextOptions<RegistrationDbContext> options) : base(options)
+        {
+        }
+        DbSet<Registration> registrations { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Registration>().HasIndex(a => a.Email).IsUnique();
+        }
+    }
+}
